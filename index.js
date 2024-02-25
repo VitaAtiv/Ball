93a310cd0d9cb0c7a2e35cbf8894b66145544640
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-02-26 00:47:14 +0200

    add

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ const refs = {
     // const y = event.clientY - refs.box.getBoundingClientRect().top;
       const x = event.clientX;
       const y = event.clientY;
-    
-    refs.ballImg.style.transform = `translate(${x}px, ${y}px)`;
+
+     refs.ballImg.style.transform = `translate(${x - 30}px, ${y - 30}px)`;
   });
+
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     // Отримати елементи DOM
+//     const box = document.querySelector(".box");
+//     const ballImg = document.querySelector(".ball-img");
+
+//     // Додати подію click на елемент .box
+//     box.addEventListener("click", function (event) {
+//       // Отримати координати кліку відносно .box
+//       const x = event.clientX - box.getBoundingClientRect().left;
+//       const y = event.clientY - box.getBoundingClientRect().top;
+
+//       // Перемістити м'яч на нові координати
+//       ballImg.style.top = y - ballImg.offsetHeight / 2 + "px";
+//       ballImg.style.left = x - ballImg.offsetWidth / 2 + "px";
+//     });
+//   });
 
 
